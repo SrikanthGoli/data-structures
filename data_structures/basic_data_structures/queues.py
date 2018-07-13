b491bf6abfed8865b5bb3d06ef9c2bae583d74cf
srikanthgoli <srikanthgoli018@gmail.com>
2018-07-12 21:32:43 -0400

    Queue Data Structure with basic operations

--- data_structures/basic_data_structures/queues.py
+++ data_structures/basic_data_structures/queues.py
@@ -1,1 +1,33 @@
 
+class queue(object):
+
+    # Queue Data Structure with basic operations
+
+    def __init__(self):
+
+        self.queue = []
+
+    # queue is empty check
+    def is_empty(self):
+
+        return self.size() == 0
+
+    # returns the size of queue
+    def size(self):
+
+        return len(self.queue)
+
+    # Adds element to queue
+    def en_queue(self, data):
+
+        self.queue.append(data)
+
+    # removed element from queue
+    def de_queue(self):
+
+        return self.queue.pop(0)
+
+    # display's the queue
+    def display(self):
+
+        return print(self.queue)
